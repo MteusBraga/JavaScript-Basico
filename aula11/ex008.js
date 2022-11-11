a9cade1516f6290f2a3802f5066721bac5c6fa3b
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2022-11-10 21:36:39 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel>60){
+    console.log(`Voce ultrapassou a velocidade limite: MULTADO`)
+}else{
+    console.log(`Dirija sempre com cinto de segurança`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = "EUA"
+
+console.log(`Voce esta morando em ${pais}`)
+
+if(pais == "Brasil"){
+    console.log('Voce é brasileiro')
+}else{
+    console.log('Voce é estrangeiro')
+}
